98c804e935fb5610fde93f7f0aef7fa69722f4b3
Unknown <ptoiraleus@gmail.com>
2018-11-06 21:52:41 +0100

    Added bonus and new graphic

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bonus : MonoBehaviour
+{
+    public float delay = 1f;
+    public GameObject bonus;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("Spawn", delay, 1);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        bonus = Instantiate(bonus, new Vector3(Random.Range(-6, 6), 10, 0), Quaternion.identity);
+        bonus.name = "Bonus";
+    }
+
+}
