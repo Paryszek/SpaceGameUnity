bc765f7427c257dfa84ff749d25478f8412c066e
Paryszek <michal.parysz@yahoo.com>
2018-11-12 09:37:58 +0100

    Falling objects fix

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class Bonus : MonoBehaviour
 {
     public GameObject bonus;
+    private float screenHalfWidth;
 
     void Start()
     {
         InvokeRepeating("Spawn", 0.01f, 3f);
+        float halfBonusWidth = transform.localScale.x / 2f;
+        screenHalfWidth = Camera.main.aspect * Camera.main.orthographicSize + halfBonusWidth;
     }
 
     void Spawn()
--- Assets/Scripts/Cubes.cs
+++ Assets/Scripts/Cubes.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class Cubes : MonoBehaviour {
     public GameObject cube;
+    private float screenHalfWidth;
 
-	void Start () {
+    void Start () {
         InvokeRepeating("Spawn", 1f, 0.7f);
+        float halfCubeWidth = transform.localScale.x / 2f;
+        screenHalfWidth = Camera.main.aspect * Camera.main.orthographicSize + halfCubeWidth;
     }
 	
 	void Spawn () {
 		if (CharacterController2D.gameOver.text != "Game Over") 
 		{
-			Instantiate(cube, new Vector3(Random.Range(-5, 3), 15, 0), Quaternion.identity);
+			Instantiate(cube, new Vector3(Random.Range(-screenHalfWidth, screenHalfWidth), 15, 0), Quaternion.identity);
 			cube.name = "Cube";
 		}
 	}
