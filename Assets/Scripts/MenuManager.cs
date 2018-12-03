4e4dcd851c548499a148c156cae6085c98226d50
Paryszek <michal.parysz@yahoo.com>
2018-12-03 09:34:36 +0100

    How to play added

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,11 +5,26 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
+    public Canvas HowToPlayCanvas;
 
+    void Start()
+    {
+        HowToPlayCanvas.gameObject.SetActive(false);
+    }
 
     public void ToGame()
     {
         Application.LoadLevel("SampleScene");
+    }
+
+    public void HowToPlay()
+    {
+        HowToPlayCanvas.gameObject.SetActive(true);
+    }
+
+    public void ExitHowToPlay()
+    {
+        HowToPlayCanvas.gameObject.SetActive(false);
     }
 
     public void EndGame()
